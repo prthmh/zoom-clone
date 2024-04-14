b3c6781eb186f5eff1efa6797e118c07bf5fd8f3
Prathmesh <111947619+prthmh@users.noreply.github.com>
2024-04-14 12:24:48 +0530

    add: new assets

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const  d = () => {
+const SideBar = () => {
   return (
-    <div> d</div>
-  )
-}
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-1 flex-col gap-6"></div>
+    </section>
+  );
+};
 
-export default  d
+export default SideBar;
---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    imgURL: "/icons/Home.svg",
+    route: "/",
+  },
+  {
+    label: "Upcoming",
+    imgURL: "/icons/upcoming.svg",
+    route: "/upcoming",
+  },
+  {
+    label: "Previous",
+    imgURL: "/icons/previous.svg",
+    route: "/previous",
+  },
+  {
+    label: "Recordings",
+    imgURL: "/icons/Video.svg",
+    route: "/recordings",
+  },
+  {
+    label: "Personal Room",
+    imgURL: "/icons/add-personal.svg",
+    route: "/personal-room",
+  },
+];
