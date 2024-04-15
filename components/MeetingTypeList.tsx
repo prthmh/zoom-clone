b6ef602d53fe888a8cc9f7858f0c1daf51027b41
Prathmesh <111947619+prthmh@users.noreply.github.com>
2024-04-15 11:12:23 +0530

    style: home page

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import HomeCard from "./HomeCard";
+
+type MeetingStateTypes =
+  | "isSchedulingMeeting"
+  | "isJoiningMeeting"
+  | "isInstantMeeitng"
+  | undefined;
+
+const MeetingTypeList = () => {
+  const router = useRouter();
+  const [meetingState, setMeetingState] =
+    useState<MeetingStateTypes>(undefined);
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeitng"
+        handleClick={() => setMeetingState("isInstantMeeitng")}
+        className="bg-orange-1"
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="via invitaiton link"
+        handleClick={() => setMeetingState("isJoiningMeeting")}
+        className="bg-blue-1"
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your next meeitng"
+        handleClick={() => setMeetingState("isSchedulingMeeting")}
+        className="bg-purple-1"
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recording"
+        description="Meeitng Recordings"
+        handleClick={() => router.push("/recordings")}
+        className="bg-yellow-1"
+      />
+    </section>
+  );
+};
+
+export default MeetingTypeList;
